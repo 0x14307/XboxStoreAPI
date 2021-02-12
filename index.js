8a4b3c9e1e695e855bf601a1c71ea3600c8b97df
0x14307 <56172613+0x14307@users.noreply.github.com>
2021-02-12 13:39:22 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const puppeteer = require('puppeteer');
+
+async function scrapeAPI(url){
+  console.log('Fetching items, this may take a bit...');
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  // Currently WIP (Needs filtering as well as making sure stuff shows up)
+
+  const nameArray = await page.evaluate(
+    () => [...document.querySelectorAll("h3.c-subheading-6")].map(partner => partner.innerText) //Gets list of all items for sale (currently only supports up to 90 per page)
+  );
+  const priceArray = await page.evaluate(
+    () => [...document.querySelectorAll("div div span.x-screen-reader")].map(partner => partner.innerText) //Gets prices (currently garbage, needs cleaning)
+  );
+  let price = priceArray;
+  price = priceArray.filter(e => e !== 'Game Pass'); // Filters out unwanted items
+
+  let filterout = price;
+  filterout = price.filter(e => e !== 'filtered by'); // Filters out "filtered by"
+
+  console.log(filterout); // Logs the fitered API response into the console & displays it 
+  browser.close();
+}
+
+scrapeAPI('https://www.microsoft.com/en-us/store/deals/games/xbox');
